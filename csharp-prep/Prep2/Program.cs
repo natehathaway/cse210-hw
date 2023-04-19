6334307f49ebc208898e0097ba13ce7f64e8d77e
Nate Hathaway <natehathaway@byui.edu>
2023-04-19 11:12:24 -0600

    finished core requirements for assignment.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage?");
+        string gradePercentage = Console.ReadLine();
+        int gradePercentageInt = int.Parse(gradePercentage);
+        string letterGrade = "";
+        if (gradePercentageInt >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (gradePercentageInt >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (gradePercentageInt >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (gradePercentageInt >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+        Console.WriteLine($"Your grade is {letterGrade}." );
+        if (gradePercentageInt >= 70)
+        {
+            Console.WriteLine("You passed, congratulations!");
+        }
+        else
+        {
+            Console.WriteLine("You failed! Better luck next time!");
+        }
     }
 }
