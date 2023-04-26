7c506d27f512ae5898f4b9200b1855aa23ad3009
Nate Hathaway <natehathaway@byui.edu>
2023-04-26 10:46:13 -0600

    Finished assignment with stretch challenges.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,51 @@
 using System;
 
-class Program
+namespace Prep3
 {
-    static void Main(string[] args)
+    public class Program
     {
-        Console.WriteLine("Hello Prep3 World!");
+        static void Main(string[] args)
+        {
+            string game = "y";
+            string loop = "y";
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 11);
+            Console.WriteLine("Welcome to the Number Guessing Game!");
+            int guessCount = 0;
+            do
+            {
+                Console.WriteLine("What is your guess?");
+                int guess = int.Parse(Console.ReadLine());
+                guessCount++;
+                if (guess == magicNumber)
+                {
+                    Console.WriteLine("You guessed it!");
+                    Console.WriteLine($"It took you {guessCount} guesses.");
+                    loop = "n";
+                    Console.WriteLine("Would you like to play again? (y/n)");
+                    game = Console.ReadLine();
+                    if (game == "y")
+                    {
+                        magicNumber = randomGenerator.Next(1, 11);
+                        guessCount = 0;
+                        loop = "y";
+                    }
+                    else
+                    {
+                        Console.WriteLine("Thanks for playing!");
+                    }
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Your guess is too high!");
+                    loop = "y";
+                }
+                else if (guess < magicNumber)
+                {
+                    Console.WriteLine("Your guess is too low!");
+                    loop = "y";
+                }
+            } while (loop == "y");
+        }
     }
 }
