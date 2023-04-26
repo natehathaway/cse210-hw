47c39ab359127441d4e32fb5b38145ae10aa8e87
Nate Hathaway <natehathaway@byui.edu>
2023-04-26 11:03:38 -0600

    finished assignment

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,20 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 class Program
 {
     static void Main(string[] args)
+    
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        List<int> numbers = new List<int>();
+        do
+            numbers.Add(int.Parse(Console.ReadLine()));
+        while (numbers[numbers.Count - 1] != 0);
+        numbers.RemoveAt(numbers.Count - 1);
+        Console.WriteLine($"The sum of the numbers is {numbers.Sum()} ");
+        Console.WriteLine($"The average of the numbers is {numbers.Average()} ");
+        Console.WriteLine($"The largest number is {numbers.Max()} ");
     }
 }
