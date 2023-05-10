a3f35d21b4dba8d7927c13c9bcea9c2065fe6686
Nate Hathaway <natehathaway@byui.edu>
2023-05-10 12:30:37 -0600

    progress on team assignment

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,9 +1,35 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+namespace Develop02
 {
-    static void Main(string[] args)
+
+    class Program
     {
-        Console.WriteLine("Hello Develop02 World!");
+        static void Main(string[] args)
+        {
+            // add some test data
+            Entry myEntry = new Entry();
+            myEntry.StorePrompt("What is your name?");
+            myEntry.StoreResponse("Nate Hathaway");
+            myEntry.StoreDate("8 May 2023");    
+
+            // create a journal to test
+            Journal myJournal = new Journal();
+            myJournal.StoreEntry(myEntry);
+
+            // show the journal contents
+            List<Entry> entries = myJournal.GetEntries();
+            foreach (Entry entry in entries)
+            {
+                string message 
+                    = $"{entry.GetDate()}\n{entry.GetPrompt()}\n{entry.GetResponse()}\n";
+                Console.WriteLine(message);
+
+            }
+
+
+
+        }
     }
 }
