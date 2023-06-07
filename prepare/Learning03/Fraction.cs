74ddfc656e6fa84c000d24ebea89d93c18af127b
Nate Hathaway <natehathaway@byui.edu>
2023-06-07 10:35:44 -0600

    prepare activity

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,41 @@
+using System;
+
+namespace Learning03
+{
+    class Fraction
+    {
+        private int _top;
+        private int _bottom;
+
+        public Fraction()
+        {
+            _top = 1;
+            _bottom = 1;
+        }
+        public Fraction(int top)
+        {
+            _top = top;
+            _bottom = 1;
+        }
+        public Fraction(int top, int bottom)
+        {
+            _top = top;
+            _bottom = bottom;
+        }
+
+        public string GetFractionString()
+        {
+            string text = $"{_top}/{_bottom}";
+            return text;
+        }
+
+        public double GetDecimalValue()
+        {
+            return (double)_top / (double)_bottom;
+        }
+
+
+
+    }
+
+}
