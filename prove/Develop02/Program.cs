24197b91e5a5a39a6af383f71d60464e03ed61da
Nate Hathaway <natehathaway@byui.edu>
2023-06-12 11:19:06 -0600

    finished journal program

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 
+
 namespace Develop02
 {
 
@@ -8,24 +9,71 @@ namespace Develop02
     {
         static void Main(string[] args)
         {
-            // add some test data
-            Entry myEntry = new Entry();
-            myEntry.StorePrompt("What is your name?");
-            myEntry.StoreResponse("Nate Hathaway");
-            myEntry.StoreDate("8 May 2023");    
-
-            // create a journal to test
-            Journal myJournal = new Journal();
-            myJournal.StoreEntry(myEntry);
-
-            // show the journal contents
-            List<Entry> entries = myJournal.GetEntries();
-            foreach (Entry entry in entries)
+           // provide a menu to the user to choose between writing and entry, displaying the entries, saving the entries, and loading the entries and a quit option.
+            Journal journal = new Journal();
+            File file = new File();
+            bool loop = true;
+            while (loop)
             {
-                string message 
-                    = $"{entry.GetDate()}\n{entry.GetPrompt()}\n{entry.GetResponse()}\n";
-                Console.WriteLine(message);
-
+            Console.WriteLine("Welcome to your journal.");
+            Console.WriteLine("What would you like to do?");
+            Console.WriteLine("1. Write an entry");
+            Console.WriteLine("2. Display Journal");
+            Console.WriteLine("3. Save Journal");
+            Console.WriteLine("4. Load Journal");
+            Console.WriteLine("5. Quit");
+            string choice = Console.ReadLine();
+            if (choice == "1")
+            {
+                Entry entry = new Entry();
+                Console.WriteLine("What is the date?(MM/DD/YYYY)");
+                string date = Console.ReadLine();
+                entry.StoreDate(date);
+                int promptNum = new Random().Next(0, 5);
+                Console.WriteLine($"{entry.prompts[promptNum]}");
+                string response = Console.ReadLine();
+                entry.StoreResponse(response);
+                journal.StoreEntry(entry);
+            }
+            else if (choice == "2")
+            {
+                List<Entry> entries2 = journal.GetEntries();
+                foreach (Entry entry in entries2)
+                {
+                    string message 
+                        = $"{entry.GetDate()}\n{entry.GetPrompt()}\n{entry.GetResponse()}\n";
+                    Console.WriteLine(message);
+                }
+            }
+            else if (choice == "3")
+            {
+                file.SaveJournal(journal);
+            }
+            else if (choice == "4")
+            {
+                journal = file.LoadJournal();
+            }
+            else if (choice == "5")
+            {
+                Console.WriteLine("Goodbye.");
+                loop = false;
+            }
+            else
+            {
+                Console.WriteLine("Invalid choice.");
+            }
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+    
             }
 
 
--- prove/Develop02/entry.cs
+++ prove/Develop02/entry.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+
+
 namespace Develop02
 {
     /// <summary>
@@ -8,6 +11,19 @@ namespace Develop02
         public string prompt;
         public string response;
         public string date;
+
+        public List<string> prompts;
+
+        public Entry()
+        {
+            prompts = new List<string>();
+            prompts.Add("Who was the most interesting person I interacted with today?");
+            prompts.Add("What was the best part of my day?");
+            prompts.Add("How did I see the hand of the Lord in my life today?");
+            prompts.Add("What was the strongest emotion I felt today?");
+            prompts.Add("If I had one thing I could do over today, what would it be?");
+            prompts.Add("What is one thing I learned today?");
+        }
 
         public string GetPrompt()
         {
